fb3dfcb6630fa62a66036408e70b536b13f5f4fb
Elias <Elias@GIANINA>
2017-03-23 17:59:33 -0600

    LOGICA

--- Subir 1/Subir 1/Form1.Designer.vb
+++ Subir 1/Subir 1/Form1.Designer.vb
@@ -22,9 +22,52 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtImprimir = New System.Windows.Forms.TextBox()
+        Me.btnResultado = New System.Windows.Forms.Button()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtImprimir
+        '
+        Me.txtImprimir.Location = New System.Drawing.Point(12, 12)
+        Me.txtImprimir.Multiline = True
+        Me.txtImprimir.Name = "txtImprimir"
+        Me.txtImprimir.Size = New System.Drawing.Size(100, 114)
+        Me.txtImprimir.TabIndex = 0
+        '
+        'btnResultado
+        '
+        Me.btnResultado.Location = New System.Drawing.Point(137, 57)
+        Me.btnResultado.Name = "btnResultado"
+        Me.btnResultado.Size = New System.Drawing.Size(75, 23)
+        Me.btnResultado.TabIndex = 1
+        Me.btnResultado.Text = "Resultado"
+        Me.btnResultado.UseVisualStyleBackColor = True
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(12, 132)
+        Me.txtResultado.Multiline = True
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(100, 117)
+        Me.txtResultado.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.btnResultado)
+        Me.Controls.Add(Me.txtImprimir)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtImprimir As TextBox
+    Friend WithEvents btnResultado As Button
+    Friend WithEvents txtResultado As TextBox
 End Class
--- Subir 1/Subir 1/Form1.vb
+++ Subir 1/Subir 1/Form1.vb
@@ -1,3 +1,48 @@
 ﻿Public Class Form1
+    Dim mat(3, 4) As Integer
+    Dim ran As New Random()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Inicializar(mat)
+        txtImprimir.Text = Imprimir(mat)
+    End Sub
+    Function Inicializar(mat(,) As Integer) As Integer(,)
+        For i = 0 To mat.GetUpperBound(0)
+            For j = 0 To mat.GetUpperBound(1)
+                mat(i, j) = ran.Next(0, 3)
+            Next
+        Next
+        Return mat
+    End Function
+    Function Imprimir(mat(,) As Integer) As String
+        Dim cadena As String = ""
+        For i = 0 To mat.GetUpperBound(0)
+            cadena += " | "
+            For j = 0 To mat.GetUpperBound(1)
+                cadena += mat(i, j).ToString() + " | "
+            Next
+            cadena += vbCrLf
+        Next
+        Return cadena
+    End Function
+    Function subir_1(mat(,) As Integer) As Integer(,)
+        Dim aux As Integer
+        Dim ultimo As Integer = mat.GetUpperBound(0) - 1
+        For i = mat.GetUpperBound(1) To mat.GetLowerBound(1)
+            For j = mat.GetUpperBound(0) To mat.GetLowerBound(0)
 
+                If mat(i, j) = 1 Then
+                    mat(i, j - 1) = aux
+                    mat(i, j) = mat(i, j - 1)
+                    aux = mat(i, j)
+                End If
+
+            Next
+
+        Next
+        Return mat
+    End Function
+
+    Private Sub btnResultado_Click(sender As Object, e As EventArgs) Handles btnResultado.Click
+        txtResultado.Text = Imprimir(subir_1(mat))
+    End Sub
 End Class
